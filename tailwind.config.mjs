a2e9f88b9b78fbd27f6a5074c5116c686a4b30cc
Kike <kike.mendoza9978@gmail.com>
2024-04-21 05:56:17 -0600

    Add proper fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'heading': ["CabinetGrotesk-Variable", "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
+			'body': ["SpaceGrotesk-Variable", "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"]
+		},
 	},
 	plugins: [],
 }
